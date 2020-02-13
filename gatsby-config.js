87933523a0906e32f27ec50a507b7cf5533f8dec
Matt Phillips <matt@mattphillips.io>
2020-02-13 22:06:42 +0000

    Add tailwindcss support

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,23 @@ module.exports = {
         codegenDelay: 200,
       }
     },
+    {
+      resolve: "gatsby-plugin-postcss",
+      options: {
+        postCssPlugins: [
+          require(`tailwindcss`)(`./tailwind.config.js`),
+          require(`autoprefixer`),
+          require(`cssnano`)
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+        purgeOnly: [`src/styles/global.css`]
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
