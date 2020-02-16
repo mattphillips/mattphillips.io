34b6837341f0091f466fad0eb79d42654e30863f
Matt Phillips <matt@mattphillips.io>
2020-02-16 18:25:01 +0000

    Add placehoder about and stats page

---
+++ src/pages/about.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { Page } from '../components/Page';
+import { Route } from '../Config';
+
+const About = () => <Page current={Route.ABOUT}>TODO: add about</Page>;
+
+export default About;
---
+++ src/pages/stats.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { Page } from '../components/Page';
+import { Route } from '../Config';
+
+const Stats = () => <Page current={Route.STATS}>TODO: add Stats</Page>;
+
+export default Stats;
