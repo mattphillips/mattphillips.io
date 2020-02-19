301cbd1fd46e06ad3fc82c119994be4580318838
Matt Phillips <matt@mattphillips.io>
2020-02-19 21:11:12 +0000

    Add sticky header detection

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,13 +5,45 @@ import styles from './post.module.css';
 
 import { BlogPostBySlugQuery } from '../../types/graphql-types';
 import { Back } from './icons/Back';
-import { Bookmark } from './icons/Bookmark';
 import { RecursiveRequired } from '../types/gql';
 import { PostMeta } from './PostMeta';
 import { Link } from './Link';
 import { Route } from '../Config';
 
 type Post = { data: RecursiveRequired<BlogPostBySlugQuery> };
+
+const Header = ({ title}: { title: string }) => {
+  const [isSticky, setIsSticky] = React.useState(false)
+  const ref = React.createRef<HTMLDivElement>()
+  
+  // mount 
+  React.useEffect(()=>{
+    const cachedRef = ref.current;
+    const observer = new IntersectionObserver(
+      ([e]) => setIsSticky(e.intersectionRatio < 1),
+      {threshold: [1]}
+    )
+
+    observer.observe(cachedRef!)
+    
+    // unmount
+    return () => {
+      observer.unobserve(cachedRef!)
+    }
+  }, [])
+
+  const styles = isSticky ? 'rounded-none shadow-lg' : 'sm:rounded-none rounded-t-xxl';
+  
+  return (
+    <div className={`${styles} p-4 sticky bg-white flex flex-row items-center z-30 w-full`} style={{top: -1}} ref={ref}>
+      <Link className="p-2" to={Route.HOME}>
+        <Back />
+      </Link>
+        {isSticky && <h6 className="mb-0 truncate" >{title}</h6>}
+    </div>
+  )
+}
+
 
 export default ({
   data: {
@@ -26,12 +58,7 @@ export default ({
 
         <div className={`h-screen absolute top-0 left-0 right-0 bottom-0 ${styles.content}`}>
           <div className="sm:rounded-t-none rounded-t-xxl bg-white -mt-8">
-            <div className="p-4 sticky top-0 bg-white sm:rounded-none rounded-t-xxl flex flex-row justify-between z-30">
-              <Link className="p-2" to={Route.HOME}>
-                <Back />
-              </Link>
-            </div>
-
+            <Header title={frontmatter.title} />
             <div className="px-6 pb-6">
               <header className="mb-6">
                 <h1 className="leading-tight mb-2">{frontmatter.title}</h1>
