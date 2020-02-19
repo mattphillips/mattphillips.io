e4f5057d20e3b99526813be995fee18b2523a43a
Matt Phillips <matt@mattphillips.io>
2020-02-19 21:11:23 +0000

    Increase font size of post metadata

--- src/components/PostMeta.tsx
+++ src/components/PostMeta.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 type PostMeta = { date: string; timeToRead: number };
 
 export const PostMeta = ({ date, timeToRead }: PostMeta) => (
-  <small className="text-xs text-gray-600 block">
+  <small className="text-sm text-gray-600 block">
     {date} • {timeToRead} min read
   </small>
 );
